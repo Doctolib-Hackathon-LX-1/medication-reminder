e9f8e01c3743f87e72e9317a2c08424c555eb59f
Arnaud Duhé <anro010@hotmail.com>
2020-06-25 12:41:22 +0100

    object done - needs to be sent to DB

--- frontend/src/components/medicationform/NewMedicationForm.js
+++ frontend/src/components/medicationform/NewMedicationForm.js
@@ -1,262 +1,54 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { useForm } from "react-hook-form";
 import SelectDateFrame from './SelectDateFrame';
 import uuid from 'uuid';
+import axios from 'axios';
 
 const NewMedicationForm = () => {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, reset } = useForm();
+    const [ reccurenceRule, setReccurenceRule ]= useState("");
+
 
     const onSubmit = (data) => {
 
+        const endTime = new Date(data.start_date);
+        endTime.setMinutes( endTime.getMinutes() + 30 );
 
-        console.log("data", typeof data.end_date, data)
         const newObject = {
             id: uuid(),
-            application_id: '4a144f5d-68ef-4784-9f52-31caea0aea68',
-            application_user_id: null,
-            title: data.name,
-            remind_on_date_tz: data.start_date,
-            remind_on_time_tz: data.end_date,
-            notify_in_advance: null, //create cariable and concatanate to have null or result as a string
-            repeat_every: null, //create cariable and concatanate to have null or result as a string
-            repeat_customized: null,
-            repeat_on_days_of_week: null,
-            repeat_on_days_of_month: null,
-            repeat_on_ordered_day_of_month_which: null,
-            repeat_on_ordered_day_of_month_day: null,
-            repeat_on_months_of_year: null,
-            start_date: data.start_date,
-            end_date: data.end_date,
+            Subject: data.treatment,
+            StartTime: new Date(data.start_date),
+            EndTime: endTime,
+            RecurrenceRule: reccurenceRule,
+            OwnerColor: '#7499E1',
         };
-        console.log("newObject", newObject);
+        reset({
+            Subject: "",
+            StartTime: "",
+            RecurrenceRule: reccurenceRule,
+            OwnerColor: ""
+        })
+        console.log('newobject', newObject)
+
+        axios.get('/api')
+        .then(res => console.log(res.data))
     };
 
-    const data = [{
-            Id: 1,
-            Subject: 'Explosion of Betelgeuse Star',
-            StartTime: new Date(2020, 5, 25, 20, 30),
-            EndTime: new Date(2020, 5, 25, 21, 0),
-            OwnerColor: '#7499e1'     
-        }, 
-        {         
-            Id: 2,         
-            Subject: 'Thule Air Crash Report',         
-            StartTime: new Date(2020, 5, 24, 22, 0),         
-            EndTime: new Date(2020, 5, 24, 22, 30),         
-            OwnerColor: '#7499e1'     
-        },
-
-        {
-            "notify_in_advance": null,
-            "repeat_every": null,
-            "repeat_customized": null,
-            "repeat_on_days_of_week": null,
-            "repeat_on_days_of_month": null,
-            "repeat_on_ordered_day_of_month_which": null,
-            "repeat_on_ordered_day_of_month_day": null,
-            "repeat_on_months_of_year": null,
-            "snoozed": 0,
-            "created_at": "2019-12-17 03:31:52",
-            "updated_at": "2019-12-17 03:31:52",
-            "last_notified_utc": null,
-            "next_notification_utc": "2020-02-15 07:30:00"
-            }]
+    const getRule = (rule) => {
+        setReccurenceRule(rule)
+    }
 
     return ( 
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-            <p>Name </p>
-                <input type="text" placeholder="Name" name="name" ref={register}/>
-            <p>Start date </p>
-                <input type="date" placeholder="start_date" name="start_date" ref={register}/>
-            <p>Start time </p>
-                <input type="time" placeholder="end_date" name="end_date" ref={register}/>
-
-                <div >
-                    <p>Notification in advance</p>
-
-
-                    <p>minutes</p><select name="minutes" ref={register}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
-                        <option value="30">30</option>
-                        <option value="11">31</option>
-                        <option value="12">32</option>
-                        <option value="13">33</option>
-                        <option value="14">34</option>
-                        <option value="15">35</option>
-                        <option value="16">36</option>
-                        <option value="17">37</option>
-                        <option value="18">38</option>
-                        <option value="19">39</option>
-                        <option value="20">40</option>
-                        <option value="21">41</option>
-                        <option value="22">42</option>
-                        <option value="23">43</option>
-                        <option value="24">44</option>
-                        <option value="25">45</option>
-                        <option value="26">46</option>
-                        <option value="27">47</option>
-                        <option value="28">48</option>
-                        <option value="29">49</option>
-                        <option value="30">50</option>
-                        <option value="31">51</option>
-                        <option value="31">52</option>
-                        <option value="23">53</option>
-                        <option value="24">54</option>
-                        <option value="25">55</option>
-                        <option value="26">56</option>
-                        <option value="27">57</option>
-                        <option value="28">58</option>
-                        <option value="29">59</option>
-                    </select>
-                    <p>hours</p><select name="hours" ref={register}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                    </select>
-                    <p>days</p><select name="days" ref={register}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
-                        <option value="30">30</option>
-                        <option value="31">31</option>
-                    </select>
-                    <p>weeks</p><select name="weeks" ref={register}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
-                        <option value="30">30</option>
-                        <option value="11">31</option>
-                        <option value="12">32</option>
-                        <option value="13">33</option>
-                        <option value="14">34</option>
-                        <option value="15">35</option>
-                        <option value="16">36</option>
-                        <option value="17">37</option>
-                        <option value="18">38</option>
-                        <option value="19">39</option>
-                        <option value="20">40</option>
-                        <option value="21">41</option>
-                        <option value="22">42</option>
-                        <option value="23">43</option>
-                        <option value="24">44</option>
-                        <option value="25">45</option>
-                        <option value="26">46</option>
-                        <option value="27">47</option>
-                        <option value="28">48</option>
-                        <option value="29">49</option>
-                        <option value="30">50</option>
-                        <option value="31">51</option>
-                        <option value="31">52</option>
-                    </select>
-                <button type="submit">Add to Calendar</button>
-            </div>
-        </div>
-        </form>
-
-            <SelectDateFrame />
+                <p>Treatment: </p>
+                <input type="text" placeholder="Treatment" name="treatment" ref={register}/>
+                <p>Starting date: </p>
+                <input type="datetime-local" placeholder="start_date" name="start_date" ref={register}/>
+            <SelectDateFrame recurrenceRule={reccurenceRule} getRule={getRule}/>
+            <button type="submit">Add to Agenda</button>
+            </form>
         </div>
      );
 }
--- frontend/src/components/medicationform/SelectDateFrame.js
+++ frontend/src/components/medicationform/SelectDateFrame.js
@@ -3,24 +3,34 @@ import { RecurrenceEditorComponent } from '@syncfusion/ej2-react-schedule';
 
 
 class SelectDateFrame extends React.Component {
+    constructor(props) {
+        super (props);
+        this.state = {
+            reccurenceRule: this.props.reccurenceRule
+        }
+    }
+
     componentDidMount() {
         let outputElement = document.querySelector('#rule-output');
         outputElement.innerText = 'Select Rule';
     }
+
     onChange(args) {
         let outputElement = document.querySelector('#rule-output');
-        console.log(args.value)
+        console.log('reccurencerule', args.value)
         if (args.value === "") {
             outputElement.innerText = 'Select Rule';
         }
         else {
             outputElement.innerText = args.value;
         }
+        
+        this.props.getRule(args.value)
     }
     
     render() {
         return <div className='content-wrapper recurrence-editor-wrap'>
-            <div style={{ paddingBottom: '15px' }}>
+            <div style={{ paddingBottom: '15px', display: 'none' }}>
             <label>Rule Output</label>
             <div className='rule-output-container'>
                 <div id='rule-output'></div>
@@ -29,7 +39,7 @@ class SelectDateFrame extends React.Component {
             <div className='RecurrenceEditor'>
             <RecurrenceEditorComponent id='RecurrenceEditor' ref={t => this.recObject = t} change={this.onChange.bind(this)}></RecurrenceEditorComponent>
             </div>
-        </div>;
+        </div>
     }
 }
 ;
