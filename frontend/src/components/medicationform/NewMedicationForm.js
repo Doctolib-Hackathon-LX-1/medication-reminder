30d2584a808db547e1954acbb3a345c95fb5bd86
amduraes <andremdpereira@gmail.com>
2020-06-25 22:34:30 +0100

    modal-popup-done

--- frontend/src/components/medicationform/NewMedicationForm.js
+++ frontend/src/components/medicationform/NewMedicationForm.js
@@ -1,79 +1,121 @@
-import React, { useState }from 'react';
-import { useForm } from "react-hook-form";
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import SelectDateFrame from './SelectDateFrame';
 import { GithubPicker } from 'react-color';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core';
 import uuid from 'uuid';
 import axios from 'axios';
+import './NewMedicationForm.scss';
 
 const NewMedicationForm = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const [reccurenceRule, setReccurenceRule] = useState('');
+  const [toggleMode, setToggleMode] = useState(false);
+  const [color, setColor] = useState();
+  const [open, setOpen] = useState(false);
 
-    const { register, handleSubmit, reset } = useForm();
-    const [ reccurenceRule, setReccurenceRule ]= useState("");
-    const [ toggleMode, setToggleMode]= useState(false);
-    const [ color, setColor]= useState();
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
 
+  const onSubmit = (data) => {
+    const endTime = new Date(data.start_date);
+    endTime.setMinutes(endTime.getMinutes() + 30);
 
-    const onSubmit = (data) => {
-
-        const endTime = new Date(data.start_date);
-        endTime.setMinutes( endTime.getMinutes() + 30 );
-
-        const newObject = {
-            medication_id: uuid(),
-            Subject: data.treatment,
-            StartTime: new Date(data.start_date),
-            EndTime: endTime,
-            RecurrenceRule: reccurenceRule,
-            OwnerColor: color,
-            user_id: 1
-        };
-        reset({
-            Subject: "",
-            StartTime: "",
-            RecurrenceRule: reccurenceRule,
-            OwnerColor: ""
-        })
-        console.log('newobject', newObject)
-
-        postNewEvent(newObject); 
-
+    const newObject = {
+      medication_id: uuid(),
+      Subject: data.treatment,
+      StartTime: new Date(data.start_date),
+      EndTime: endTime,
+      RecurrenceRule: reccurenceRule,
+      OwnerColor: color,
+      user_id: 1,
     };
+    reset({
+      Subject: '',
+      StartTime: '',
+      RecurrenceRule: reccurenceRule,
+      OwnerColor: '',
+    });
+    console.log('newobject', newObject);
 
-    const postNewEvent = (newObject) => {
-        axios
-        .post('/newmedication', newObject)
-        .then((response) => console.log(response));
-    }
+    postNewEvent(newObject);
+    handleClickOpen();
+  };
 
-    const handleClick = () => {
-        setToggleMode(!toggleMode);
-    }
+  const postNewEvent = (newObject) => {
+    axios
+      .post('/newmedication', newObject)
+      .then((response) => console.log(response));
+  };
 
-    const handleChangeComplete = (color, event) => {
-        setColor(color.hex);
-        setToggleMode(!toggleMode);
-    };
+  const handleClick = () => {
+    setToggleMode(!toggleMode);
+  };
 
-    const getRule = (rule) => {
-        setReccurenceRule(rule)
-    }
+  const handleChangeComplete = (color, event) => {
+    setColor(color.hex);
+    setToggleMode(!toggleMode);
+  };
 
-    return ( 
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <p>Treatment: </p>
-                <button onClick={handleClick}>{!toggleMode && 'Pick a color'}</button>
-               {toggleMode && 
-                <GithubPicker onChangeComplete={handleChangeComplete}/>}
-                <input type="text" placeholder="Treatment" name="treatment" ref={register}/>
-                <p>Starting date: </p>
-                <input type="datetime-local" placeholder="start_date" name="start_date" ref={register}/>
-                <SelectDateFrame recurrenceRule={reccurenceRule} getRule={getRule}/>
-                <button type="submit">Add to Agenda</button>
-            </form>
-        </div>
-     );
-}
- 
+  const getRule = (rule) => {
+    setReccurenceRule(rule);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <p>Treatment: </p>
+        <button onClick={handleClick}>{!toggleMode && 'Pick a color'}</button>
+        {toggleMode && <GithubPicker onChangeComplete={handleChangeComplete} />}
+        <input
+          type='text'
+          placeholder='Treatment'
+          name='treatment'
+          ref={register}
+        />
+        <p>Starting date: </p>
+        <input
+          type='datetime-local'
+          placeholder='start_date'
+          name='start_date'
+          ref={register}
+        />
+        <SelectDateFrame recurrenceRule={reccurenceRule} getRule={getRule} />
+        <button type='submit'>Add to Agenda</button>
+      </form>
+      <div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby='alert-dialog-title'
+          aria-describedby='alert-dialog-description'>
+          <DialogTitle id='alert-dialog-title'>
+            {'Your treatment has been successfully created.'}
+          </DialogTitle>
+          <DialogActions>
+            <Link to='/agenda' className='modal-ok-link'>
+              <Button onClick={handleClose} color='primary' autoFocus>
+                OK
+              </Button>
+            </Link>
+          </DialogActions>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
+
 export default NewMedicationForm;
