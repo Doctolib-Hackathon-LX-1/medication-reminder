620c0dfb82bc5125f4c10f35c39dbecf9b8b8845
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-06-25 16:30:15 +0100

    calendar done

--- frontend/src/components/agenda/Agenda.js
+++ frontend/src/components/agenda/Agenda.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Agenda.css';
 import { ScheduleComponent, Inject, Day, Week, WorkWeek, Month, Agenda, ViewsDirective, ViewDirective, ResourcesDirective, ResourceDirective} from '@syncfusion/ej2-react-schedule';
-import { extend, L10n } from '@syncfusion/ej2-base';
+// import { Internationalization, extend, L10n } from '@syncfusion/ej2-base';
+import { Internationalization, extend } from '@syncfusion/ej2-base';
 
 const AgendaPage = (props) => {
     const data = [
@@ -11,27 +12,23 @@ const AgendaPage = (props) => {
         StartTime: new Date(2020, 5, 24, 20, 30),
         EndTime: new Date(2020, 5, 24, 21, 0),
         RecurrenceRule: 'FREQ=WEEKLY;BYDAY=TU,WE,FR,SA;INTERVAL=1;COUNT=13;',
-        PrimaryColor: "#3484c2",
-        SecondaryColor: "#3484c2",
+        OwnerColor: "#d9edff"
     }, 
     {
         Id: 2,
-        Subject: 'Old',
+        Subject: 'Test',
         StartTime: new Date('2020-06-25T13:30'),
         EndTime: new Date('2020-06-25T14:00'),
         RecurrenceRule: 'FREQ=DAILY;INTERVAL=1;COUNT=2',
-        PrimaryColor: "#8a1c3d",
-        SecondaryColor: "#8a1c3d",
+        OwnerColor: "#fcebb3",
     },
     {
         Id: 3,
-        Subject: "New Medication",
+        Subject: "Medication",
         StartTime: "Tue Jun 24 2020 14:45:00 GMT+0100 (heure d’été d’Europe de l’Ouest)",
         EndTime: "Tue Jun 24 2020 15:15:00 GMT+0100 (heure d’été d’Europe de l’Ouest)",
         RecurrenceRule: "FREQ=DAILY;INTERVAL=1;COUNT=5;",
-        OwnerColor: '#8a1c3d',
-        PrimaryColor: "#8a1c3d",
-        SecondaryColor: "#8a1c3d",
+        OwnerColor: '#ffd7cf',
     }
 ]
     const [calendarData, setCalendarData] = useState(data);
@@ -54,42 +51,19 @@ const AgendaPage = (props) => {
     //     args.cancel = true;
     // }
 
-    // const onPopupOpen = (args) => {
-    //     if (args.type === 'EventContainer') {
-    //         args.cancel = true;
-    //     }
-    // }
-
-    // const onEventRendered = (args) => {
-    //     applyCategoryColor(args, scheduleObj.currentView);
-    // }
-
-    // const applyCategoryColor = (args, currentView) => {
-    //     let categoryColor = args.data.CategoryColor;
-    //     if (!args.element || !categoryColor) {
-    //         return;
-    //     }
-    //     if (currentView === 'Agenda') {
-    //         args.element.firstChild.style.borderLeftColor = categoryColor;
-    //     }
-    //     else {
-    //         args.element.style.backgroundColor = categoryColor;
-    //     }
-    // }
-
-    // const eventTemplate = (data) => {
-    //     console.log("template", data)
-    //     return (
-    //         <div className="template-wrap" style={{ backgroundColor: calendarData.SecondaryColor }}>
-    //             <div className="subject" style={{ backgroundColor: calendarData.PrimaryColor }}>
-    //                 {calendarData.Subject}
-    //             </div>
-    //             <div className="time" style={{ backgroundColor: calendarData.PrimaryColor }}>
-    //                 Time: {calendarData.StartTime} - {calendarData.props.EndTime}
-    //             </div>
-    //         </div>
-    //     );
-    // }
+    const instance = new Internationalization();
+    const getTimeString = (value) => {
+        return instance.formatDate(value, { skeleton: 'hm' });
+    }
+    const eventTemplate = (props) => {
+        return (
+            <div className="template-wrap" style={{ backgroundColor: props.OwnerColor, padding: '0px 5px', color: 'black'}}>
+                <div className="subject" style={{ backgroundColor: props.OwnerColor, fontWeight: '500' }}>{props.Subject}</div>
+                <div className="time" style={{ backgroundColor: props.OwnerColor }}>
+                Time: {getTimeString(props.StartTime)} - {getTimeString(props.EndTime)}</div>
+            </div>
+        );
+    }
 
     return ( 
         <div className="Agenda">
@@ -101,11 +75,8 @@ const AgendaPage = (props) => {
                     <ScheduleComponent 
                         height="500px"
                         selectedDate={new Date()}
-                        eventSettings={{ dataSource: calendarData }}
-                        colorField='OwnerColor'
-                        // rowAutoHeight={true}
-                        // ref={schedule => this.scheduleObj = schedule}
-                        // eventRendered={onEventRendered}
+                        eventSettings={{ dataSource: calendarData, template: eventTemplate, enableTooltip: true }}
+                        rowAutoHeight={true}
                         >
                         <ViewsDirective>
                             <ViewDirective option="Day"></ViewDirective>
