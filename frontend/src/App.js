56ce3b7b7e211653503ea1638e5e7a9893c23568
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-06-26 01:00:33 +0100

    css styles and aboutUs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import MedicationContextProvider from './context/MedicationContext';
 import HomePage from './components/home/HomePage';
 import SetMedication from './components/medicationform/SetMedication';
 import AgendaPage from './components/agenda/AgendaPage';
+import About from './components/about/About';
 
 function App() {
   // Example Connection to the Express Server
@@ -24,6 +25,7 @@ function App() {
             <Route exact path='/' component={HomePage} />
             <Route exact path='/medicationform' component={SetMedication} />
             <Route exact path='/agenda' component={AgendaPage} />
+            <Route exact path='/about' component={About} />
           </Switch>
         </MedicationContextProvider>
       </BrowserRouter>
